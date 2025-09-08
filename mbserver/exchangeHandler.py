fe6d90f9a9e23ae26ba422764d7048a0f851ef88
goodsirhello807 <goodsirhello807@gmail.com>
2025-09-08 06:47:15 +0000

    Added the Queue class to work with queues

--- mbserver/exchangeHandler.py
+++ mbserver/exchangeHandler.py
@@ -7,3 +7,36 @@ class Message:
     def __init__(self, messageId: str = "", head=False):
         self.messageId = 0 if head else messageId
         self.next = None
+
+class Queue:
+    def __init__(self, queueName):
+        self.queueName = queueName
+        self.count = 0
+        self.head = Message(head=True)
+        self.tail = None
+    
+    def addMessage(self, messageId):
+        newMessage = Message(messageId)
+        if self.tail is None:
+            self.tail = newMessage
+            self.head.next = self.tail
+        
+        else:
+            self.tail.next = newMessage
+            self.tail = newMessage 
+        
+    def popMessage(self):
+        print("Removing Message")
+        if self.head.next is None:
+            raise NoMessageException()
+        
+        message = self.head.next
+        self.head.next = self.head.next.next
+        if self.head.next is None:
+            self.tail = None
+
+        return message.messageId
+
+
+
+
