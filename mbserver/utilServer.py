8af8db2efdd96164b03b0d3afbc4a9892ee2d163
goodsirhello807 <goodsirhello807@gmail.com>
2025-09-08 06:24:59 +0000

    Joined the mb-sockets with utilServer and added them in lifecycle- Worked on only default exchange

--- mbserver/utilServer.py
+++ mbserver/utilServer.py
@@ -1,11 +1,36 @@
 
+import time
+import asyncio
+from multiprocessing import Process
 from fastapi import FastAPI
+from exchangeHandler import Exchange
 from routes import login, uiPage
 
 app = FastAPI()
 
 app.include_router(login.router, tags=["Auth"])
 app.include_router(uiPage.router, tags=["Admin-UI"])
+
+exchange_process = None
+
+def start_exchange():
+    exchange = Exchange(hostName="server01", exchangeName="default", port=48001, queues=[""])
+    asyncio.run(exchange.handleSocket())  # Runs the async socket server
+
+@app.on_event("startup")
+async def startup_event():
+    global exchange_process
+    exchange_process = Process(target=start_exchange)
+    exchange_process.start()
+    time.sleep(2)
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    global exchange_process
+    if exchange_process:
+        exchange_process.terminate()
+        exchange_process.join()
+        print("Exchange process terminated")
 
 @app.get("/")
 async def getHomePage():
@@ -17,3 +42,4 @@ async def getHomePage():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("utilServer:app", host="0.0.0.0", port=42425, reload=True)
+    
