791514ac1a0f57209e661080829f419dd6fc96b5
goodsirhello807 <goodsirhello807@gmail.com>
2025-09-09 19:03:27 +0000

    Added functionality to start and stop process(working on stopExchange)

---
+++ mbserver/utils/utilsHelper.py
@@ -0,0 +1,35 @@
+
+import asyncio
+from exchangeHandler import Exchange
+
+def start_exchange(args):
+    hostName=args["hostName"]
+    exchangeName=args["exchangeName"]
+    port=args["port"]
+    queues=args["queues"]
+    terminateSwitch=args["terminateSwitch"]
+
+    exchange = Exchange(hostName=hostName, exchangeName=exchangeName, port=port, queues=queues, terminateSwitch=terminateSwitch)
+    asyncio.run(exchange.handleSocket())  # Runs the async socket server
+
+async def stopExchange(exchangeName, exchanges):
+    if exchangeName not in exchanges:
+        return True
+
+    print("Terminating Exchange :", exchangeName)
+
+    exchangeObject, exchangeTerminateSwitch = exchanges[exchangeName]
+    exchangeTerminateSwitch.set()
+    exchangeObject.kill()
+    exchangeObject.join(timeout=1)
+
+    if exchangeObject.is_alive():
+        print("Process is still alive")
+        exchangeObject.kill()
+        exchangeObject.join()
+
+    del(exchanges[exchangeName])
+    return True
+
+
+
